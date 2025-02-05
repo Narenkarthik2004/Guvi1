05cf9818deea255856940ab3089e02904b5e78e3
NK <112756092+Narenkarthik2004@users.noreply.github.com>
2025-02-05 10:28:29 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u naren1023 -p Naren@2004
-    docker tag test naren1023/docker_guvi
-    docker push naren1023/docker_guvi
-    
